a28d7d14fa92f86e46e12d29c96b170556fa7a1a
Rui Xiao <ruixiao85@gmail.com>
2021-03-01 03:32:52 -0600

    indentation and typo fix.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs= require('fs');
 const express=require('express')
 const app=express()
 const PORT=3000
-app.listen(PORT, ()=>console.log(`Listerning on Port ${PORT}`))
+app.listen(PORT, ()=>console.log(`Listerning on localhost:${PORT}`))
 
 // app.use('/www', express.static(path.join(__dirname, 'www')))
 app.use(express.static(__dirname))
@@ -40,74 +40,73 @@ const html_head=function(title){ return(/*html*/`
 <!DOCTYPE html/>
 <html>
 <head>
-<title>${title}</title>
-    <link rel="shortcut icon" href="" >
-    <link rel="stylesheet" href="www/bootstrap400.min.css"/>
-    <link rel="stylesheet" href="www/bootstrap-toc.css"/>
-    <script src="www/jquery-3.2.1.slim.min.js"></script>
-    <script src="www/d3.v3.min.js"></script>
-    <script src="www/bootstrap400.min.js"></script>
-    <script src="www/bootstrap-toc.js"></script>
-    <script>
-      const topN=10; // only show first few rows for the data table
-    </script>
-  </head>
-
-  <body data-spy="scroll" data-target="#toc">
+  <title>${title}</title>
+  <link rel="shortcut icon" href="" >
+  <link rel="stylesheet" href="www/bootstrap400.min.css"/>
+  <link rel="stylesheet" href="www/bootstrap-toc.css"/>
+  <script src="www/jquery-3.2.1.slim.min.js"></script>
+  <script src="www/d3.v3.min.js"></script>
+  <script src="www/bootstrap400.min.js"></script>
+  <script src="www/bootstrap-toc.js"></script>
+  <script>
+    const topN=10; // only show first few rows for the data table
+  </script>
+</head>
+<body data-spy="scroll" data-target="#toc">
   <div class="container">
   <div class="row">
   <div class="col-sm-3">
-  <nav id="toc" data-toggle="toc" class="sticky-top"></nav>
+    <nav id="toc" data-toggle="toc" class="sticky-top"></nav>
   </div>
   <div class="col-sm-9">
   <caption>${title}</caption>
-  `)};
+`)};
 
 const html_foot=function(){ return(/*html*/`
-</div>
-<script type="text/javascript">
-  $('img[data-enlargeable]').addClass('img-enlargeable').click(function(){
-    var src = $(this).attr('src');
-    var modal;
-    function removeModal(){ modal.remove(); $('body').off('keyup.modal-close'); }
-    modal = $('<div>').css({
-      background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
-      backgroundSize: 'contain',
-      width:'100%', height:'100%',
-      position:'fixed',
-      zIndex:'10000',
-      top:'0', left:'0',
-      cursor: 'zoom-out'
-    }).click(function(){ removeModal(); }).appendTo('body');
-    $('body').on('keyup.modal-close', function(e){ if(e.key==='Escape'){ removeModal(); } });
-  });
+  </div>
+  <script type="text/javascript">
+    $('img[data-enlargeable]').addClass('img-enlargeable').click(function(){
+      var src = $(this).attr('src');
+      var modal;
+      function removeModal(){ modal.remove(); $('body').off('keyup.modal-close'); }
+      modal = $('<div>').css({
+        background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+        backgroundSize: 'contain',
+        width:'100%', height:'100%',
+        position:'fixed',
+        zIndex:'10000',
+        top:'0', left:'0',
+        cursor: 'zoom-out'
+      }).click(function(){ removeModal(); }).appendTo('body');
+      $('body').on('keyup.modal-close', function(e){ if(e.key==='Escape'){ removeModal(); } });
+    });
   </script>
-<script type="text/javascript">
-  function load_table(path) {
-    var eid=path.replace(/[/\.]/g,"-")
-    d3.text(path, function(data){
-      var rows = d3.csv.parseRows(data);
-      var tab=d3.select("#"+eid);
-      if (tab.select("tr").empty()) { // if empty, add
-        tab.append("caption").text(path+" Top "+topN)
-        tab.append("thead").append("tr")
-        .selectAll("th")
-        .data(rows[0])
-        .enter().append("th")
-        .text(function(d) { return d; });
-        tab.append("tbody")
-        .selectAll("tr").data(rows.slice(1,1+topN))
-        .enter().append("tr")
-        .selectAll("td")
-        .data(function(d){ return d; })
-        .enter().append("td")
-        .text(function(d){ return d; })
-      } else {
-        tab.selectAll("*").remove(); // if exist, clean
-      }
-    })
-  }
-</script>
+  <script type="text/javascript">
+    function load_table(path) {
+      var eid=path.replace(/[/\.]/g,"-")
+      d3.text(path, function(data){
+        var rows = d3.csv.parseRows(data);
+        var tab=d3.select("#"+eid);
+        if (tab.select("tr").empty()) { // if empty, add
+          tab.append("caption").text(path+" Top "+topN)
+          tab.append("thead").append("tr")
+          .selectAll("th")
+          .data(rows[0])
+          .enter().append("th")
+          .text(function(d) { return d; });
+          tab.append("tbody")
+          .selectAll("tr").data(rows.slice(1,1+topN))
+          .enter().append("tr")
+          .selectAll("td")
+          .data(function(d){ return d; })
+          .enter().append("td")
+          .text(function(d){ return d; })
+        } else {
+          tab.selectAll("*").remove(); // if exist, clean
+        }
+      })
+    }
+  </script>
 </body>
 </html>
 `)};
